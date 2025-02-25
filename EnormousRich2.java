1c359f86dddc1db2a8b4560d6c592e616ccdf44e
ekdus04 <cr0a_@naver.com>
2025-02-25 22:05:52 +0900

    Create EnormousRich2.java

--- EnormousRich2.java
+++ EnormousRich2.java
@@ -1,5 +1,11 @@
-public class EnormousRich2 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class EnormousRich2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(n / m);
+        System.out.println(n % m);
     }
 }
